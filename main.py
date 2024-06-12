cd228299250ab38ffa0ca126bcfb248c74713e2c
mikheil godoladze <mikheil.godoladze@yahoo.com>
2024-06-12 22:25:57 +0400

    main.py changes

--- main.py
+++ main.py
@@ -10,13 +10,35 @@ def main():
     audit_planning = AuditPlanning()
     work_process = WorkProcess()
     reporting = Reporting()
-    
-    # Example usage
-    audit_register.register_audit()
-    audit_group.create_group()
-    audit_planning.plan_audit()
-    work_process.process_work()
-    reporting.generate_report()
+
+    while True:
+        try:
+            choice = input(''' ::::::Audit Process Management ::::::: 
+            1. Register Audit
+            2. Create Audit Group
+            3. Plan Audit
+            4. Process Audit Work
+            5. Generate Report
+            6. Exit
+            Choose Option: ''')
+
+            if choice == '1':
+                audit_register.register_audit()
+            elif choice == '2':
+                audit_group.create_group()
+            elif choice == '3':
+                audit_planning.plan_audit()
+            elif choice == '4':
+                work_process.process_work()
+            elif choice == '5':
+                reporting.generate_report()
+            elif choice == '6':
+                print("Exiting program.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
